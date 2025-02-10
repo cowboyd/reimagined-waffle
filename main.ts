96f86d9c61f55b98d089db2155d730cc174495e0
Charles Lowell <cowboyd@frontside.com>
2025-02-10 16:06:08 -0600

    Add workflows for node as a control

--- main.ts
+++ main.ts
@@ -28,17 +28,8 @@ if (typeof result === "string") {
   console.log(
     "fail: tried to send a kill to child process and await close event, but it did not happen",
   );
-  Deno.exit(1);
+  process.exit(1);
 } else {
   console.log("success");
-  Deno.exit(0);
-}
-
-export function add(a: number, b: number): number {
-  return a + b;
-}
-
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+  process.exit(0);
 }
